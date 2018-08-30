e6c2d4ceeabf19d7824661adf8f2b4823fbea41a
sky <dejiafang@gmail.com>
2018-08-30 23:54:50 +0800

    部署到rinkeby测试网络

--- truffle.js
+++ truffle.js
@@ -1,4 +1,17 @@
+require('babel-register');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic="engage solve diet staff equal pulp guess album engine reject treat brief";
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*"
+        },
+        rinkeby: {
+            provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/",0),
+            network_id: 4,
+            gas: 4612388
+        }
+    }
 };
